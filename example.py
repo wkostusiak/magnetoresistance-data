7ab7ca90ddcbb054442102c5a1cd8d2ca7bb7913
wkostusiak <wkostusiak@gmail.com>
2023-02-01 22:19:07 +0100

    separation based on temperature sign, code made clearer

--- example.py
+++ example.py
@@ -1,16 +1,18 @@
-from .functions import separate_position,position_csv, fast_separate, slow_separate, separate_temp
+from .functions import separate_pos,position_csv, fast_csv, slow_csv, temp_csv, signs_csv
 
+# working example below #
 
-dane_90st = separate_position('dane', 90.0144)
-dane_0st = separate_position('dane', 0)
+dane_90st = separate_pos('dane', 90.0144)
+dane_0st = separate_pos('dane', 0)
 
-parent_var = 'C:\\Users\\lenovo\\Desktop\\ziombal'
+position_csv(dane_0st, 'C:\\Users\\lenovo\\Desktop\\magnetoresistance', 'dane_0', 'dane_0' )
+position_csv(dane_90st, 'C:\\Users\\lenovo\\Desktop\\magnetoresistance', 'dane_90', 'dane_90' )
 
-position_csv(dane_0st, 'C:\\Users\\lenovo\\Desktop\\ziombal', 'dane_0', 'dane_0_all' )
-position_csv(dane_90st, 'C:\\Users\\lenovo\\Desktop\\ziombal', 'dane_90', 'dane_90_all' )
+fast_measurement = fast_csv(dane_0st, -0.179859027266502, 'dane_0')
+fast_measurement = fast_measurement[4:]
+slow_measurement = slow_csv(fast_measurement, dane_0st, 'dane_0')
 
-fast_measurement = fast_separate(dane_0st, -0.179859027266502, 'dane_0')
-fast_measurement = fast_measurement[4:] # manually checked the data --> first 4 rows had to be deleted #
-slowmeasurement = slow_separate(fast_measurement, dane_0st, 'dane_0')
+names = temp_csv(slow_measurement, 'C:\\Users\\lenovo\\Desktop\\magnetoresistance\\dane_0')
+signs_csv('C:\\Users\\lenovo\\Desktop\\magnetoresistance\\dane_0', names)
 
-separate_temp(slowmeasurement, dirname='dane_0')
+# results in dane_0 and dane_90 dirs in repo #
--- functions.py
+++ functions.py
@@ -3,45 +3,46 @@ import pandas
 import numpy as np
 
 
-# choosing data for 90 or 0 deg position #
+# separating data based on sample position #
+# position: int value, name: assumed that extension is .dat #
 
-
-def separate_position(name, position):
+def separate_pos(name, position):
     filename = '{}.dat'.format(name)
     data = pandas.read_csv(filename)
-    dane_st = data[data['Position (Deg)'] == position]
+    data_st = data[data['Position (Deg)'] == position]
 
-    return dane_st
+    return data_st
 
 
 # mkdir to organize files #
-# parent - path to project folder, dirname - folder we want to create, filename - the csv file #
+# parent - path to project folder, dirname - name of a folder we want to create #
 
 
-def makedir(parent, dirname, filename):
+def makedir(parent, dirname):
     path = os.path.join(parent, dirname)
     try:
         os.mkdir(path)
     except FileExistsError:
         pass
-    filepath = os.path.join(path, filename)
 
-    return filepath
-
-
-# create csv file in new directory#
+    return path
 
 
-def position_csv(dane_st, parent_var, dirname, filename):
-    dirname = str(dirname)
+# creation of a csv file in new directory based on sample position #
+
+
+def position_csv(dane_st, parent, dirname, filename):
+    path = makedir(parent, dirname)
     name = '{}.csv'.format(filename)
-    dane_st.to_csv(makedir(parent_var, dirname, name), index=False)
+    filepath = os.path.join(path, name)
+    dane_st.to_csv((filepath), index=False)
 
 
-# saves row for constant field fast measurement to csv file and returns dataframe
+# saving rows for constant field measurement to csv file and returning dataframe #
+# field: int value #
 
 
-def fast_separate(data, field, dirname):
+def fast_csv(data, field, dirname):
     rounded_temp = data['Map23 (K)'].round(2)
     data['rounded temp'] = rounded_temp
     df_diff = (data['rounded temp'] - data['rounded temp'].shift(1))
@@ -52,25 +53,26 @@ def fast_separate(data, field, dirname):
     return fastmeasurement
 
 
-# manually check if fastmeasurement does not contain slow measurement for a given field #
+# manually check if fastmeasurement does not contain slow measurements for a given field! #
+# saving rows for alternating field measurement to csv file and returning dataframe #
 
 
-def slow_separate(fastmeasurement, data, dirname):
+def slow_csv(fastmeasurement, data, dirname):
     slowmeasurement = pandas.concat([fastmeasurement, data], ignore_index=True).drop_duplicates(keep=False)
     slowmeasurement.to_csv(f'{dirname}/slowmeasurement.csv', index=True)
 
     return slowmeasurement
 
 
-# selecting by temperature: 2, 4, 6, 8, 10, 25, 50, 100
-# searching for change points in the file
+# selecting by temperature and saving to the new directory for each temp value #
 
 
-def separate_temp(slowmeasurement, dirname):
+def temp_csv(slowmeasurement, parent):
     change_points = slowmeasurement.loc[
         (slowmeasurement['diff'].abs() >= 1) | (slowmeasurement['diff'].apply(np.isnan))]
     change_points.sort_index(axis=0)
     indexes = change_points.index.tolist()
+
     names = []
     for index in indexes:
         names.append((change_points['Map23 (K)'][index]).round(1))
@@ -78,15 +80,37 @@ def separate_temp(slowmeasurement, dirname):
     i = 0
     for index in indexes:
 
-        name = '{}/{} K.csv'.format(dirname, names[i])
+        name = '{} K.csv'.format(names[i])
+        newdir = '{} K'.format(names[i])
+        path = makedir(parent, newdir)
+        filepath = os.path.join(path, name)
 
         if index == indexes[-1]:
 
             newdf = slowmeasurement.loc[indexes[-1]:]
-            newdf.to_csv(name, index=False)
+            newdf.to_csv(filepath, index=False)
         else:
             newdf = slowmeasurement.loc[indexes[i]: ((indexes[i + 1]) - 1)]
-            newdf.to_csv(name, index=False)
+            newdf.to_csv(filepath, index=False)
         i += 1
+        # newdf.to_csv(name, index=False)?
+
+    return names
+
+
+# separating each temperature csv file depending on [Oe] sign #
+# creating csv files for positive and negative values in previously created directory #
+def signs_csv(inputpath, names):
+    for name in names:
+        dirpath = '{}/{} K'.format(inputpath, name)
+        inputfilepath = '{}/{} K.csv'.format(dirpath, name)
+
+        data = pandas.read_csv(inputfilepath)
+        neg_K = data[data['Field (Oe)'] < 0]
+        neg_name = '{}/{}_neg.csv'.format(dirpath, name)
+        neg_K.to_csv(neg_name, index=False)
+        pos_K = data[data['Field (Oe)'] > 0]
+        pos_name = '{}/{}_pos.csv'.format(dirpath, name)
+        pos_K.to_csv(pos_name, index=False)
 
     return None
